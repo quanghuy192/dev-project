6e94347da993f4a915cb711e53b991c2e8ca6141
huy <quanghuydang1902@gmail.com>
2022-08-12 02:38:15 +0700

    - update login, index

--- src/main/java/com/myproject/devproject/WebController.java
+++ src/main/java/com/myproject/devproject/WebController.java
@@ -1,13 +1,18 @@
 package com.myproject.devproject;
 
+import com.myproject.devproject.models.Movie;
 import com.myproject.devproject.models.User;
+import com.myproject.devproject.services.MovieService;
 import com.myproject.devproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -15,15 +20,21 @@ public class WebController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    MovieService movieService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        final List<Movie> movies = movieService.findAll();
+
+        model.addAttribute("movies", movies);
         return "index";
     }
 
-    @PostMapping("/users")
-    public String registerUsers(@RequestBody User user) {
+    @PostMapping("/user")
+    public String register(@RequestBody User user) {
         userService.resolveUser(user);
-        return "index";
+        return "logout";
     }
 
     @GetMapping("/users")
@@ -32,8 +43,8 @@ public class WebController {
         return "index";
     }
 
-    @GetMapping("/user/{username}")
-    public String getUser(@PathVariable String username) {
+    @PostMapping("/user/{username}")
+    public String login(@PathVariable String username) {
         userService.findBy(username);
         return "index";
     }
